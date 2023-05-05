ce8f757d8826d0ec266a4ec9a1931f35bee87fc8
Khôi Nguyễn <khoih232@gmail.com>
2023-05-05 15:15:02 +0700

    Nop

---
+++ js/java.js
@@ -0,0 +1,14 @@
+let btn_DangKi =document.getElementById("btn_DangKi");
+let form =document.getElementById("from");
+
+btn_DangKi.addEventListener('click', function(){
+
+   
+    form.classList.add('show');
+
+});
+
+let btn_Luu =document.getElementById('btnLuu');
+
+
+
